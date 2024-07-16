ae21113b16d0ee9c62cd75e6d95c1f7b8dbca53f
Comfort Mwalija <calmwalija@gmail.com>
2024-07-16 08:42:56 +0200

    [Quest/Malawi Core] Bug Fix: Unable to open save Guardian Registration Questionnaires when the patient is on ART due to FHIR Engine Library update

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -195,7 +195,7 @@ dependencies {
 
   api("org.smartregister:common:0.1.0-alpha05-preview3-SNAPSHOT") { isTransitive = true }
 
-  api("org.smartregister:engine:1.0.0-preview11-MWCore-SNAPSHOT") {
+  api("org.smartregister:engine:1.0.0-preview10-PERF-TEST-SNAPSHOT") {
     isTransitive = true
     exclude(group = "com.google.android.fhir", module = "common")
     exclude(group = "com.github.ben-manes.caffeine")
