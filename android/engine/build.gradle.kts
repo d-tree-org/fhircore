30e973ac91426ca8b7b8865e2e2c23406b75c217
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-07-22 08:51:15 +0200

    update transform support service
    update transform support service
    
    Update build.gradle.kts

--- android/engine/build.gradle.kts
+++ android/engine/build.gradle.kts
@@ -185,7 +185,7 @@ dependencies {
     exclude(group = "com.google.android.fhir", module = "common")
     exclude(group = "com.google.android.fhir", module = "engine")
   }
-  api("org.smartregister:data-capture:1.1.0-preview8.1-SNAPSHOT") {
+  api("org.smartregister:data-capture:1.1.0-preview11-SNAPSHOT") {
     isTransitive = true
     exclude(group = "ca.uhn.hapi.fhir")
     exclude(group = "com.google.android.fhir", module = "engine")
@@ -195,7 +195,7 @@ dependencies {
 
   api("org.smartregister:common:0.1.0-alpha05-preview3-SNAPSHOT") { isTransitive = true }
 
-  api("org.smartregister:engine:1.0.0-preview10-PERF-TEST-SNAPSHOT") {
+  api("org.smartregister:engine:1.0.0-preview11-MWCore-SNAPSHOT") {
     isTransitive = true
     exclude(group = "com.google.android.fhir", module = "common")
     exclude(group = "com.github.ben-manes.caffeine")
