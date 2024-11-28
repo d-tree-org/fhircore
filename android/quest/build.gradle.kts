a5418ea929693d40a25b5c2223759d8a953cbd39
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-11-28 10:02:23 +0200

    Production release 0.2.0.3

--- android/quest/build.gradle.kts
+++ android/quest/build.gradle.kts
@@ -139,22 +139,22 @@ android {
       dimension = "apps"
       applicationIdSuffix = ".mwcore"
       versionNameSuffix = "-mwcore"
-      versionCode = 38
-      versionName = "0.2.0.2"
+      versionCode = 39
+      versionName = "0.2.0.3"
     }
     create("mwcoreDev") {
       dimension = "apps"
       applicationIdSuffix = ".mwcoreDev"
       versionNameSuffix = "-mwcoreDev"
-      versionCode = 38
-      versionName = "0.2.0.2"
+      versionCode = 39
+      versionName = "0.2.0.3"
     }
     create("mwcoreStaging") {
       dimension = "apps"
       applicationIdSuffix = ".mwcoreStaging"
       versionNameSuffix = "-mwcoreStaging"
-      versionCode = 38
-      versionName = "0.2.0.2"
+      versionCode = 39
+      versionName = "0.2.0.3"
     }
   }
 
